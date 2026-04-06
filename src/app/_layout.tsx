fed51079df3a87379f8c45ffb4445dfc0c839025
tobsBP <tobias.bp2004@gmail.com>
2026-04-06 20:18:38 -0300

    feat: tanstack query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,24 @@
 import '../../global.css';
+import { onlineManager, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as Network from 'expo-network';
 import { Stack } from 'expo-router';
+
+const queryClient = new QueryClient();
+
+onlineManager.setEventListener((setOnline) => {
+  const subscription = Network.addNetworkStateListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+  return subscription.remove;
+});
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="(auth)" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
